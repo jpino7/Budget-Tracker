74e058812e52f38f72c146c6ba31e266a13b281f
JRPino <pino3789@gmail.com>
2020-06-13 12:58:52 -0700

    Added useState to Form.js and verified in Components - hooks that it is connected correctly

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const Form = () => {
+    const [text, setText] = useState('');
+    const [amount, setAmount] = useState(0);
+
     return (
         <div>
                 <h3>Add Transaction</h3>
                 <form id="form">
                     <div className="form-control">
                         <label htmlFor="text">Transaction: </label>
-                        <input type="text" placeholder="Enter Transaction Type" />
+                        <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter Transaction Type" />
                     </div>
                     <div className="form-control">
                         <label htmlFor="amount">Amount [ (-) Withdraw | (+) Deposit ]: </label>
-                        <input type="number" placeholder="Enter Amount"/>
+                        <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter Amount"/>
                     </div>
                     <button className="addTransaction-btn">Add Transaction</button>
                 </form>
