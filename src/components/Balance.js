d405c09fa4f08120e96e9c2015645a0c2179a683
JRPino <pino3789@gmail.com>
2020-06-15 12:08:52 -0700

    Changed id for balance to className=balance

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -4,7 +4,7 @@ export const Balance = () => {
     return (
         <div>
             <h2>Balance:</h2>
-            <h3 id="balance">$0.00</h3>
+            <h3 className="balance">$0.00</h3>
         </div>
     )
 }
