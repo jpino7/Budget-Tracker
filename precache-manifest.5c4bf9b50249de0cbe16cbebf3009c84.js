1468f3c0a58938ea4721797facc625b671b2eca4
JRPino <pino3789@gmail.com>
2020-06-15 17:09:49 -0700

    Updates

---
+++ precache-manifest.5c4bf9b50249de0cbe16cbebf3009c84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a98337901a00d61dce1be4dfd01cd14",
+    "url": "/Budget-Tracker/index.html"
+  },
+  {
+    "revision": "26bba707de136f35afae",
+    "url": "/Budget-Tracker/static/css/main.c6774afe.chunk.css"
+  },
+  {
+    "revision": "3aabf21996142c1dd831",
+    "url": "/Budget-Tracker/static/js/2.3410e46d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Budget-Tracker/static/js/2.3410e46d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26bba707de136f35afae",
+    "url": "/Budget-Tracker/static/js/main.945e55ab.chunk.js"
+  },
+  {
+    "revision": "4f58b4deffdfc0bd394a",
+    "url": "/Budget-Tracker/static/js/runtime-main.a609487d.js"
+  }
+]);
